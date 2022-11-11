8be126c52c2c271398ee820416e1e9af46403260
restelli436 <117980583+restelli436@users.noreply.github.com>
2022-11-11 12:52:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+
+float a, b, c;
+
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
 
+float Areatriangolo = (a+b) / 2;
+float Areaquadrato = (a * a);
+float Arearettangolo = (a * b);
+float Areatrapezio = ((a+b) * c)/2;
+
+cout << Areatriangolo << endl;
+cout << Areaquadrato << endl;
+cout << Arearettangolo << endl;
+cout << Areatrapezio << endl;
+
+return 0;
 
 }
